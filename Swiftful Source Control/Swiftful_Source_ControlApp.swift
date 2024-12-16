2230100e2e9552be0ade5ad0da6617338a49c46f
Vytenis Lengvinas <wytenis90@gmail.com>
2024-12-16 14:10:56 +0200

    Initial Commit

---
+++ Swiftful Source Control/Swiftful_Source_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftful_Source_ControlApp.swift
+//  Swiftful Source Control
+//
+//  Created by vytenis lengvinas on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Swiftful_Source_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
